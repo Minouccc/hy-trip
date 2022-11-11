d39df3fa364f3ab5e7a8ac968ac982fede6d6915
Minouccc <343723899@qq.com>
2022-11-11 19:11:20 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app">
+    <router-view />
+    <tar-bar />
+  </div>
+</template>
+<script setup>
+import TarBar from "@/components/tab-bar/tab-bar.vue";
+</script>
+<style scoped></style>
